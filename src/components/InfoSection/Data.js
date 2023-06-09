4d49956b28b58f6aa409050912b6c36a87959f4d
lamayassergln <glnlamayasser@yahoo.com>
2023-06-09 07:18:49 +0300

    first commit

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne={
+    id:'about',
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topLine:'Premium Bank',
+    headline:'Unlimited Transactions with zero fees',
+    description:'Get acces to our exclusive app that allow you to send unlimited transactions without getting charged any fees',
+    buttonLabel:'Get Started',
+    imgStart:false,
+    img: require('../../images/svg-1.svg').default,
+    alt:'Car',
+    dark:true,
+    primary:true,
+    darkText:false
+}
+
+export const homeObjTwo={
+    id:'discover',
+    lightBg:true,
+    lightText:false,
+    lightTextDesc:false,
+    topLine:'Unlimited Access',
+    headline:'Unlimited Transactions with zero fees',
+    description:'Get acces to our exclusive app that allow you to send unlimited transactions without getting charged any fees',
+    buttonLabel:'Learn More',
+    imgStart:true,
+    img: require('../../images/svg-2.svg').default,
+    alt:'Piggybank',
+    dark:false,
+    primary:false,
+    darkText:true
+}
+
+export const homeObjThree={
+    id:'signup',
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topLine:'Join our Team',
+    headline:'Unlimited Transactions with zero fees',
+    description:'Get acces to our exclusive app that allow you to send unlimited transactions without getting charged any fees',
+    buttonLabel:'Start now',
+    imgStart:false,
+    img: require('../../images/svg-3.svg').default,
+    alt:'paper',
+    dark:true,
+    primary:true,
+    darkText:false
+}
