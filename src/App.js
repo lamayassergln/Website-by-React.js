d0765f604ed388045e613d423b33ea44da489600
lamayassergln <glnlamayasser@yahoo.com>
2023-06-09 09:44:08 +0300

    Deployed website

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home/>}exact />
+        <Route path='/Website-by-React.js' element={<Home/>}exact />
         <Route path='/signin' element={<SigninPage/>} exact/>
       </Routes>
     </Router>
